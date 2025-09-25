446e62a1dcaacd719f038b75ad352fd88871b1a9
heeyeon6635 <145956376+heeyeon6635@users.noreply.github.com>
2025-09-25 11:08:09 +0900

    Update main.js to file path

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ここからコードを書いてください
 
 // 1. tabs.js 에서 setupTabs 함수 import
-import { setupTabs } from "./tabs.js";
-import { setupConverter } from "./converter.js";
+import { setupTabs } from "./js/tabs.js";
+import { setupConverter } from "./js/converter.js";
 
 // 2. DOMContentLoaded 이벤트 발생 시 setupTabs 실행
 // document.addEventListener("이벤트이름", 콜백함수);
